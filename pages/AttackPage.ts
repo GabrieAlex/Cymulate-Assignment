4e6c087c03c8d17b52d82f79aa1fa72f3c86e179
aleex <alee.xx1987@gmail.com>
2025-05-05 21:55:47 +0300

    add page new page

--- pages/AttackPage.ts
+++ pages/AttackPage.ts
@@ -1,22 +1,22 @@
 import { Page, Locator, expect } from '@playwright/test';
+import { Console } from 'console';
 
 export class AttackPage {
 
   private assessmentIdElements: Locator;
-
+ 
   constructor(protected page: Page) {
 
     this.assessmentIdElements = this.page.locator('div[test-data-id="assessmentID"]');
-
-  }
-
- public async waitForAttackIds(timeout: number = 20000){
-
-    await this.assessmentIdElements.first().waitFor({timeout});
-    
+  
   }
 
   public async getUniqueAttackIds() {
+
+    await this.assessmentIdElements.first().waitFor();
+    await this.assessmentIdElements.last().scrollIntoViewIfNeeded();
+    await this.page.waitForTimeout(2000);
+
     const uniqueIds = new Set<string>();
     const count = await this.assessmentIdElements.count();
 
@@ -26,28 +26,11 @@ export class AttackPage {
         uniqueIds.add(id.trim());
       }
     }
-
     return Array.from(uniqueIds);
   }
 
-  /**
-   * Logs all unique attack IDs to the console.
-   */
   public async logAttackIds(){
     const uniqueIds = await this.getUniqueAttackIds();
-    console.log(`Found ${uniqueIds.length} unique Attack IDs:`, uniqueIds);
+    console.log('First 3 Attack IDs:', uniqueIds.slice(0, 3));
   }
-
-  /**
-   * Asserts that at least one Attack ID is present.
-   */
-  public async assertAtLeastOneAttackId(){
-    const uniqueIds = await this.getUniqueAttackIds();
-    expect(uniqueIds.length).toBeGreaterThanOrEqual(4);
-       // Get and log unique attack IDs
-       console.log('Unique Attack IDs:', uniqueIds);
-       // Optionally, print the first 3
-       console.log('First 3 Attack IDs:', uniqueIds.slice(0, 4));
-  }
-  
 }
